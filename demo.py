bae2fbac8bc3a8dd118f4bf747e43f167938b1e1
sheetal29coder <sheetalsen8085@gmail.com>
2025-08-10 10:17:30 +0530

    add file

--- demo.py
+++ demo.py
@@ -1,13 +1,8 @@
-"""
-This module contains test cases for the application.
-"""
+def hello():
+    """
+    this is demo
+    """
+    return "hwlo dosoto"
 
 
-def test_sample():
-   """A sample test function."""
-   assert 1 + 1 == 2
-
-
-if __name__ == "__main__":
-    test_sample()
-    print("All tests passed.")
+hello()
